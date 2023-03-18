4d0bf887ec53db3c3fbbd9015f1faf2ffafe7f59
pkbike01 <pankajbikerider07@gmail.com>
2023-03-19 01:30:12 +0530

    second commit

---
+++ src/main/java/com/apiDevlopment2/qoreConfig/apiConfig.java
@@ -0,0 +1,43 @@
+package com.apiDevlopment2.qoreConfig;
+
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+
+//@ConfigurationProperties(prefix = "api")
+//public class apiConfig {
+//	private String value1;
+//
+//	public String getValue1() {
+//		return value1;
+//	}
+//
+//	public void setValue1(String value1) {
+//		this.value1 = value1;
+//	}
+//	
+//	
+//	
+//
+//}
+
+
+//@ConfigurationProperties(prefix = "myapp")
+//public class MyAppConfig {
+//    private String myVariable1;
+//    private String myVariable2;
+//
+//    public String getMyVariable1() {
+//        return myVariable1;
+//    }
+//
+//    public void setMyVariable1(String myVariable1) {
+//        this.myVariable1 = myVariable1;
+//    }
+//
+//    public String getMyVariable2() {
+//        return myVariable2;
+//    }
+//
+//    public void setMyVariable2(String myVariable2) {
+//        this.myVariable2 = myVariable2;
+//    }
+//}
--- src/main/java/com/apiDevlopment2/services/LeadService_Impl.java
+++ src/main/java/com/apiDevlopment2/services/LeadService_Impl.java
@@ -3,18 +3,38 @@ package com.apiDevlopment2.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.apiDevlopment2.entities.Lead;
+
 import com.apiDevlopment2.repository.LeadRepository;
 
 @Service
 public class LeadService_Impl implements LeadService {
+//	 	@Autowired
+//	    private apiConfig myAppConfig;
+
+//	    public void doSomething() {
+//	        String myVariable1 = myAppConfig.getValue1();
+////	        String myVariable2 = myAppConfig.getMyVariable2();
+//	        // Use myVariable1 and myVariable2
+//	        
+//	    }
+//	 private String v1 = "qinsight";
+//	 private String v2 = myVariable1;
+	
 	@Autowired
 	private LeadRepository leadRepo;
 	
+	
+	@Value("${api.value1}")
+	private String v1;
+	
+	
 	@Override
 	public void saveOneLead(Lead lead) {
+		System.out.println(v1);
 		leadRepo.save(lead);
 		
 		
@@ -23,6 +43,7 @@ public class LeadService_Impl implements LeadService {
 	@Override
 	public List<Lead> listLeads() {
 		List<Lead> leads = leadRepo.findAll();
+		System.out.println(v1);
 		return leads;
 	}
 
